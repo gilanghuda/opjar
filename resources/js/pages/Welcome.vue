bae433fbdccdcc901c64ce8efbc7533fd5abd7c4
gilanghuda <gilanghuda99@gmail.com>
2026-01-02 19:07:52 +0700

    refactor(page): update dashboard and welcome pages

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { dashboard, login, register } from '@/routes';
+import { home, login, register } from '@/routes';
 import { Head, Link } from '@inertiajs/vue3';
 
 withDefaults(
@@ -24,9 +24,9 @@ withDefaults(
             class="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl"
         >
             <nav class="flex items-center justify-end gap-4">
-                <Link
+                    <Link
                     v-if="$page.props.auth.user"
-                    :href="dashboard()"
+                    :href="home()"
                     class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                 >
                     Dashboard
